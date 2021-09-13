7b2670f77610cb1fb1c072ca210852f70bea8e48
Dmitry Garbuz <dgarbuz1888@gmail.com>
2021-09-13 16:44:19 +0300

    fix Sashas fail)

---
+++ src/Components/Title/styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const TitleWrapper = styled.h3`
+  font-family: 'Lato-Bold';
+  font-style: normal;
+  font-weight: 700;
+  display: inline-block;
+  letter-spacing: 0.05em;
+  color: ${(p) => p.color};
+  font-size: ${(p) => p.fontSize};
+`;
