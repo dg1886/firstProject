c8c75564298cc201843b5a6349868c5743407d2a
Dmitry Garbuz <dgarbuz1888@gmail.com>
2021-09-13 13:16:41 +0300

    - fixed RegistrationForm layout;

--- src/Components/RegistrationForm/Form.jsx
+++ src/Components/RegistrationForm/Form.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
-import { FormAreaWrapper, FormPageWrapper } from './styled';
+import { PageWrapper, FormWrapper } from './styled';
 import Button from '../Button';
 import Input from '../Inputs/Input';
 import Title from '../Title/Title';
 
 const RegistrationForm = () => (
-  <FormPageWrapper>
-    <FormAreaWrapper>
+  <PageWrapper>
+    <FormWrapper>
       <Title fontSize="2em" />
-      <form>
-        <Input width="600px" type="email" placeholder="Email" />
-        <Input width="600px" type="password" placeholder="Password" />
-        <Input width="600px" type="password" placeholder="Confirm password" />
-        <Button width="300px" backgroundColor="#006162" color="#f4f9f9" />
-      </form>
-    </FormAreaWrapper>
-  </FormPageWrapper>
+      <Input width="600px" type="email" placeholder="Email" />
+      <Input width="600px" type="password" placeholder="Password" />
+      <Input width="600px" type="password" placeholder="Confirm password" />
+      <Button width="300px" backgroundColor="#006162" color="#f4f9f9" />
+    </FormWrapper>
+
+  </PageWrapper>
 );
 
 export default RegistrationForm;
--- src/Components/RegistrationForm/styled.js
+++ src/Components/RegistrationForm/styled.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const FormPageWrapper = styled.div`
+export const PageWrapper = styled.div`
   width: 100%;
   height: 100vh;
   background-color: rgba(222, 220, 220, 0.78);
@@ -10,14 +10,14 @@ export const FormPageWrapper = styled.div`
   align-items: center;
 `;
 
-export const FormAreaWrapper = styled.div`
-  height: 80%;
-  width: 50%;
-  background-color: #eff3f3;
-  border-radius: 8px;
-  box-shadow: 5px 5px 16px 2px rgba(14, 29, 52, 0.15);
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  align-items: center;
+export const FormWrapper = styled.form`
+    height: 80%;
+    width: 50%;
+    background-color: #eff3f3;
+    border-radius: 8px;
+    box-shadow: 5px 5px 16px 2px rgba(14, 29, 52, 0.15);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
 `;
