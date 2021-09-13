36fa4e602338746648e071b6e3fe9c99ad37824b
Dmitry Garbuz <dgarbuz1888@gmail.com>
2021-09-13 14:59:49 +0300

    - fixed;

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-// import GlobalFonts from './Fonts/Fonts';
 
 import RegistrationForm from './Components/RegistrationForm/Form';
 
 function App() {
   return (
     <div className="App">
-      {/* <GlobalFonts /> */}
       <RegistrationForm />
     </div>
   );
