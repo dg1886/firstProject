c118947f7750105933ab47df9f630f1cd1f1d7b7
Dmitry Garbuz <dgarbuz1888@gmail.com>
2021-09-20 18:37:26 +0300

    - api request in HomePage; - create and use preloader; - install axios deps;

---
+++ src/Common/Preloader/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import preloaderImg from '../../Img/Preloader.svg';
+import { PreloadImg } from './styled';
+
+const Preloader = () => <PreloadImg src={preloaderImg} alt="...loading" />;
+
+export default Preloader;
